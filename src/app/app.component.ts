fbf9ca6b13f9a6541ff7edfd31467f0111531ff4
Yufan <yuxu1@users.noreply.github.com>
2024-04-07 13:09:01 -0700

    added movie card component(button,dialog) to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -23,5 +24,10 @@ export class AppComponent {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
     })
+  }
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
   }
 }
