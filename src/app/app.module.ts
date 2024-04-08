843a46eb66317343ef90ac9f08aca65bf3ebc5e4
Yufan <yuxu1@users.noreply.github.com>
2024-04-07 18:47:31 -0700

    create movie card buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 import { RouterModule, Routes } from '@angular/router';
-
+//for implementing Add to Favorites icon
+import { MatIconModule } from '@angular/material/icon';
 
 //define routing
 const appRoutes: Routes = [
@@ -47,7 +49,8 @@ const appRoutes: Routes = [
     MatDialogModule ,
     MatSnackBarModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MatIconModule
   ],
   providers: [
     provideClientHydration(),
